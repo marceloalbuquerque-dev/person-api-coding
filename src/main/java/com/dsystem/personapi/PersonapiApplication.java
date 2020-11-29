c508bf71fb689a9a438ece15a71012e0a0c5505b
Marcelo Albuquerque <marceloradeveloper@gmail.com>
2020-11-28 23:52:29 -0300

    Project created

---
+++ src/main/java/com/dsystem/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package com.dsystem.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsystem/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsystem.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
